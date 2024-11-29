fdf649a9c766ec982d870894677fcd4159171b0f
Pritam <107666898+pritam12112001@users.noreply.github.com>
2024-11-29 17:09:59 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index1.html'],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#ee0181'
+      },
+      fontFamily:{
+        'personal-font': ["Lobster", "serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
